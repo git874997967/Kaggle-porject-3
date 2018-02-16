02d59360cd57938df8e41803288a864c6c02df0a
zhenming <yangz2@kec1130-18.engr.oregonstate.edu>
2018-02-16 13:19:59 -0800

    the best score get add one more factor to predict also use ranger instead of randomforest for better performance

--- bikeSharing1.r
+++ bikeSharing1.r
@@ -1,7 +1,5 @@
 library(rpart)
 library(rpart.plot)
-library(mice)
-library(VIM)
 library(ranger)
 library(randomForest)
 library(car)
@@ -9,8 +7,8 @@ library(lubridate)
 library(dplyr)
 train = read.csv("train.csv")
 test = read.csv("test.csv")
-train$label='train'
-test$label='test'
+train$label = 'train'
+test$label = 'test'
 # understand the data set
 # datetime - hourly date + timestamp
 # season -  1 = spring, 2 = summer, 3 = fall, 4 = winter
@@ -42,15 +40,17 @@ data = bind_rows(train, test)
 str(data)
 ## 添加派生变量
 data$hour1 = as.factor(hour(data$datetime))
-data$hour2 = substr(data$datetime, 12, 13)
+# data$hour2 = substr(data$datetime, 12, 13)
 data$day = weekdays(as.Date(data$datetime))
-data$day[data$day == "星期六"] = "Saturday"
-data$day[data$day == '星期日'] = "Sunday"
-data$day[data$day == "星期一"] = "Monday"
-data$day[data$day == '星期二'] = "Tuesday"
-data$day[data$day == "星期三"] = "Wednesday"
-data$day[data$day == '星期四'] = "Thursday"
-data$day[data$day == "星期五"] = "Friday"
+data$test = data$temp * data$humidity
+# in chinese envoronment use code below
+# data$day[data$day == "星期六"] = "Saturday"
+# data$day[data$day == '星期日'] = "Sunday"
+# data$day[data$day == "星期一"] = "Monday"
+# data$day[data$day == '星期二'] = "Tuesday"
+# data$day[data$day == "星期三"] = "Wednesday"
+# data$day[data$day == '星期四'] = "Thursday"
+# data$day[data$day == "星期五"] = "Friday"
 
 data$year = year(data$datetime)
 ## 产生皮尔逊相关参数
@@ -76,139 +76,74 @@ rpart.plot(d)
 
 #### 将连续型变量变成离散型变量  借助决策树模型
 data$temp_reg = 0
-data$temp_reg[data$hour1%in%c(0,1,2,3,4,5,6,7,8,9,23,21,22)]=1
-data$temp_reg[data$hour1%in%c(10:20)]=2
-# data$temp_reg[data$hour1 == 0 |
-#                 data$hour1 == 1 |
-#                 data$hour1 == 2 |
-#                 data$hour1 == 3 |
-#                 data$hour1 == 4 |
-#                 data$hour1 == 5 |
-#                 data$hour1 == 6 |
-#                 data$hour1 == 7 |
-#                 data$hour1 == 8 |
-#                 data$hour1 == 9 |
-#                 data$hour1 == 21 | data$hour1 == 22 |
-#                 data$hour1 == 23] = 1
-
-# data$temp_reg[data$hour1 == 10 |
-#                 data$hour1 == 11 |
-#                 data$hour1 == 12 |
-#                 data$hour1 == 13 |
-#                 data$hour1 == 14 |
-#                 data$hour1 == 15 |
-#                 data$hour1 == 16 |
-#                 data$hour1 == 17 |
-#                 data$hour1 == 18 | data$hour1 == 19 |
-#                 data$hour1 == 20] = 2
-
+data$temp_reg[data$hour1 %in% c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 21, 22)] =
+  1
+data$temp_reg[data$hour1 %in% c(10:20)] = 2
 
 
 
 ###
-data$dp_reg[data$hour1%in%c(0,1,2,3,4,5,6,23)]=1
-data$dp_reg[data$hour1%in%c(10,11,21,22)]=2
-data$dp_reg[data$hour1%in%c(7,9,12,13,14,15,20)]=3
-data$dp_reg[data$hour1%in%c(16,19)]=4
-data$dp_reg[data$hour1%in%c(8,17,18)]=5
-# data$dp_reg[data$hour1 == 0 |
-#               data$hour1 == 1 |
-#               data$hour1 == 2 |
-#               data$hour1 == 3 |
-#               data$hour1 == 4 | data$hour1 == 5 | data$hour1 == 6 |
-#               data$hour1 == 23] = 1
-# data$dp_reg[data$hour1 == 10 |
-#               data$hour1 == 11 |
-#               data$hour1 == 21 | data$hour1 == 22] = 2
-# data$dp_reg[data$hour1 == 7 |
-#               data$hour1 == 9 |
-#               data$hour1 == 12 |
-#               data$hour1 == 13 |
-#               data$hour1 == 14 | data$hour1 == 15 |
-#               data$hour1 == 20] = 3
-# data$dp_reg[data$hour1 == 16 | data$hour1 == 19] = 4
-# data$dp_reg[data$hour1 == 8 |
-#               data$hour1 == 17 | data$hour1 == 18] = 5
-# data$dp_reg[data$hour1>=20&data$hour1<22]=6
-# data$dp_reg[data$hour1>=18&data$hour1<20]=7
-data$dp_cas = 0
-# data$dp_cas[data$hour1 == 0 |
-#               data$hour1 == 1 |
-#               data$hour1 == 2 |
-#               data$hour1 == 3 |
-#               data$hour1 == 4 |
-#               data$hour1 == 5 | data$hour1 == 6 | data$hour1 == 7 |
-#               data$hour1 == 23] = 1
+data$dp_reg[data$hour1 %in% c(0, 1, 2, 3, 4, 5, 6, 23)] = 1
+data$dp_reg[data$hour1 %in% c(10, 11, 21, 22)] = 2
+data$dp_reg[data$hour1 %in% c(7, 9, 12, 13, 14, 15, 20)] = 3
+data$dp_reg[data$hour1 %in% c(16, 19)] = 4
+data$dp_reg[data$hour1 %in% c(8, 17, 18)] = 5
 
-data$dp_cas[data$hour1%in%c(0,1,2,3,4,5,6,7,23)]=1
-data$dp_cas[data$hour1%in%c(8,9,20,21,22)]=2
-data$dp_cas[data$hour1%in%c(10,11,18,19,12)]=3
-data$dp_cas[data$hour1%in%c(12,13,14,15,16,17)]=4
- 
-# data$dp_cas[data$hour1 == 8 |
-#               data$hour1 == 9 |
-#               data$hour1 == 20 | data$hour1 == 21 |
-#               data$hour1 == 22] = 2
-# data$dp_cas[data$hour1 == 10 |
-#               data$hour1 == 11 |
-#               data$hour1 == 18 | data$hour1 == 19] = 3
-# data$dp_cas[data$hour1 == 12 |
-#               data$hour1 == 13 |
-#               data$hour1 == 14 |
-#               data$hour1 == 15 | data$hour1 == 16 |
-#               data$hour1 == 17] = 4
+data$dp_cas = 0
+
+
+data$dp_cas[data$hour1 %in% c(0, 1, 2, 3, 4, 5, 6, 7, 23)] = 1
+data$dp_cas[data$hour1 %in% c(8, 9, 20, 21, 22)] = 2
+data$dp_cas[data$hour1 %in% c(10, 11, 18, 19, 12)] = 3
+data$dp_cas[data$hour1 %in% c(12, 13, 14, 15, 16, 17)] = 4
+
+
 ### 增加日期类型
 data$daytype = ""
 data$daytype[data$holiday == 1] = "holiday"
 data$daytype[data$workingday == 1] = "workingday"
 data$daytype[data$holiday == 0 & data$workingday == 0] = "weekend"
-data$weekend=0
-data$weekend[data$daytype=="weekend"]=1
- 
- 
+data$weekend = 0
+data$weekend[data$daytype == "weekend"] = 1
+## add test for temp*humidity
+
+data$tlevel = 2
+data$tlevel[data$hour1 %in% c(11:19)] = 1
+
 ###因子化 变量
 data$hour1 = as.factor(data$hour1)
 data$day = as.factor(data$day)
 data$daytype = as.factor(data$daytype)
 set.seed(1234)
-train=data[data$label=='train',] 
-test=data[data$label=='test',]
+set.seed(451)
+train = data[data$label == 'train', ]
+test = data[data$label == 'test', ]
 
-str(train) 
 
-
+str(train)
 train$logreg = log(train$registered + 1)
 train$logcas = log(train$casual + 1)
 fit1 = ranger(
-  formula = logreg ~ hour1 + workingday + day +weekend + holiday  + temp_reg + humidity +
-    atemp + windspeed + season + weather + dp_reg +  year,
+  formula = logreg ~ hour1 + workingday + day + weekend + holiday  + temp_reg + humidity +
+    atemp + windspeed + season + weather + dp_reg +  year + tlevel + test,
   data = train,
-  num.trees = 250
-  
+  num.trees = 3000,
+  num.threads = 8
 )
 fit2 = ranger(
-  formula =logcas ~ hour1 + workingday + day+weekend  + holiday  + temp_reg + humidity +
-    atemp + windspeed + season + weather + dp_cas +  year,
+  formula = logcas ~ hour1 + workingday + day + weekend  + holiday  + temp_reg + humidity +
+    atemp + windspeed + season + weather + dp_cas +  year + tlevel + test,
   data = train,
-  num.trees = 250
-   
+  num.trees = 3000,
+  num.threads = 8
 )
-
-
-p1=predict(fit1,test)
-test$logreg=p1
-p2=predict(fit2,test)
-test$logcas=p2
+p1 = predict(fit1, test)
+test$logreg = p1$predictions
+p2 = predict(fit2, test)
+test$logcas = p2$predictions
 head(test)
-test$registered=exp(test$logreg)
-test$casual=exp(test$logcas)
-test$count=test$registered+test$casual
-submit_final=data.frame(datetime=test$datetime,count=as.integer(test$count))
-write.csv(submit_final,"sub1.csv")
-head(submit_final,20)
-# 
-# head(submit_final$datatime)
-# head(submit_final)
-# t1=read.csv("randomForest.csv")
-# str(t1)
-# str(submit_final)
+test$registered = exp(test$logreg)
+test$casual = exp(test$logcas)
+test$count = test$registered + test$casual
+submit_final = data.frame(datetime = test$datetime, count = as.integer(test$count))
+write.csv(submit_final, "sub4.csv")
